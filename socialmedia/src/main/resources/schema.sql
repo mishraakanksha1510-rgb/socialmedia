cacd26d2dd68a65a1e8ab6f1df3af3111433f137
Akanksha <mishraakanksha1510@gmail.com>
2026-09-03 21:30:12 +0530

    Add database schema

---
+++ socialmedia/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone VARCHAR(15) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
